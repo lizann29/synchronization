510912acc21f4b8096315f09b228d92003ef27a0
Lizann29 <lizininua2001@gmail.com>
2023-10-13 09:57:43 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'synchronization';
+
+  constructor(private http: HttpClient) {} 
+
+  fetchData() {
+    const url = ''; 
+
+    this.http.get(url).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
+
+
+
